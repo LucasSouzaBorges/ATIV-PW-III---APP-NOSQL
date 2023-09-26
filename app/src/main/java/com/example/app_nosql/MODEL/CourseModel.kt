8d51778f6055e93850b7b128571a61df6f07ed7d
LucasSouzaBorges <lucas.borges90@etec.sp.gov.br>
2023-09-25 23:44:23 -0300

    APP - NOSQL

---
+++ app/src/main/java/com/example/app_nosql/MODEL/CourseModel.kt
@@ -0,0 +1,8 @@
+package com.example.app_nosql.MODEL
+
+data class CourseModel(
+    var courseName: String,
+    var courseDuration: String,
+    var courseTracks: String,
+    var courseDescription: String
+)
